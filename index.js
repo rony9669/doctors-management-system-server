1e718fb2ce074b8ca4a23fc37018e3760fd1b4d8
rony9669 <ronychowdhury.cse@gmail.com>
2022-12-02 00:59:59 +0600

    mongodb settings added

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 //mdb start
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gavhqqs.mongodb.net/?retryWrites=true&w=majority`;
-// console.log(uri);
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
